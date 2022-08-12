ec30b5fa3d9a152d42a7a562fa6fa7ebd4b147af
Marie Dépalle <m.depalle87@gmail.com>
2022-08-12 17:13:05 +0200

    création du footer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import GlobalStyles from './global-styles'
+import 'normalize.css';
+import {GlobalStyles} from './global-styles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(<>
+  <GlobalStyles/>
   <App />
+</>
+  
 );
 
 
